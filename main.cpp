100aba04e0bb668a46f007332aedc0aebdb6fc0b
Riley Kirkham <rileykirkham@engr2-0-199-dhcp.int.colorado.edu>
2020-02-10 17:04:21 -0700

    Adding main, which include unit tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "functions_to_implement.cpp"
 
 int main(){
     std::cout<<"This is main."<<std::endl;
+    //Testing RemoveAllSubstrings()
+    std::cout<<RemoveAllSubstrings("abcWow this is a abcreally abccool stringabc" "abc")<<std::endl;
+    //Should print " Wow this is a really cool string".
+    
+    //Testing Join()
+    //Should pring "I wow really wow like wow writing wow strings wow"
+    std::vector<string> hmm = ("I"," really", " like", " writing", " strings")
+    std::cout<< Join(hmm, "wow")<<std::endl;
+
 }
